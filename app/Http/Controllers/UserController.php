2603b3a5c67bd1063ebfe4a119caf93ea0dd170d
Admin <admin@example.com>
2021-04-21 17:25:14 +0800

    Changes as of April 19,2021

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,14 @@
     namespace App\Http\Controllers;
 
     use Illuminate\Http\Request;
-    use App\User;
+    use Illuminate\Http\Response;
+    use App\Models\User;
+    use App\Traits\ApiResponser;
+    use DB;
 
     Class UserController extends Controller {
+        use ApiResponser;
+
         private $request;
 
         public function __construct(Request $request){
@@ -13,8 +18,67 @@
         }
 
         public function getUsers(){
+            //$users = User::all();
+            $users = DB::connection('mysql')
+            ->select("Select * from tbluser");
+
+            //return response()->json($users, 200);
+            return $this->successResponse($users);
+        }
+
+        public function index(){
             $users = User::all();
-            return response()->json($users, 200);
+            return $this->successResponse($users);
+        }
+
+        public function addUser(Request $request) {
+            $rules = [
+                'username' => 'required|max:255',
+                'password' => 'required|max:255',
+            ];
+            
+            $this->validate($request, $rules);
+            $users = User::create($request->all());
+            return $this->successResponse($users, Response::HTTP_CREATED);
+        }
+
+        public function show($id) {
+            $users = User::where('id', $id)->first();
+            if ($users){
+                return $this->successResponse($users);
+            }
+            return $this->errorResponse('User ID Does Not Exist', Response::HTTP_NOT_FOUND);
+        }
+        public function update(Request $request, $id) {
+            $rules = [
+                'username' => 'max:255',
+                'password' => 'max:255',
+                'admin' => 'in:1,0',
+            ];
+
+            $this->validate($request, $rules);
+
+            $users = User::where('id', $id)->first();
+
+                if($users){
+                    $users->fill($request->all());
+                    if($users->isClean()) {
+                        return $this->errorResponse('At least one value must be changed',
+                        Response::HTTP_UNPROCESSABLE_ENTITY);
+                    }
+                    $users->save();
+                    return $this->successResponse($users);
+                }
+                return $this->errorResponse('User ID does not exist', Response::HTTP_NOT_FOUND);
+        }
+
+        public function delete($id) {
+            $users = User::where('id', $id)->first();
+            if($users){
+                $users->delete();
+                return $this->successResponse($users);
+            }
+            return $this->errorResponse('User ID does not exist', Response::HTTP_NOT_FOUND);
         }
     }
 
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,9 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('/users',['uses' => 'UserController@getUsers']);
 });
    
+$router->get('/users', 'UserController@index');
+$router->post('/users', 'UserController@addUser');
+$router->get('/users/{id}', 'UserController@show');
+$router->put('/users/{id}', 'UserController@update');
+$router->patch('/users/{id}', 'UserController@update');
+$router->delete('/users/{id}', 'UserController@delete');
